d29be747b6fbda0079de469d2dd7a60863a40160
ian <kickthechair@gmail.com>
2022-02-15 17:58:32 +0000

    Feature test passed. Player 1 can select 'scissors' as a move and get a confirmation message back of their choice. Refactored all feature tests regarding player move choice into one feature test file as they are related

--- spec/feature/player_can_select_rock_spec.rb
+++ spec/feature/player_can_select_rock_spec.rb
@@ -8,4 +8,20 @@ feature 'select a move' do
     expect(page).to have_content('You selected \'Rock\'!')
   end
 
+  scenario 'choose paper and get a confirmation message' do
+    enter_and_submit_name
+    display_player_moves
+    choose('Paper')
+    click_button('Submit')
+    expect(page).to have_content('You selected \'Paper\'!')
+  end
+
+  scenario 'choose scissors and get a confirmation message' do
+    enter_and_submit_name
+    display_player_moves
+    choose('Scissors')
+    click_button('Submit')
+    expect(page).to have_content('You selected \'Scissors\'!')
+  end
+
 end
