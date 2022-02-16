706e62fcf995c3d7f72ab02b1bafa7b563487e65
ian <kickthechair@gmail.com>
2022-02-16 17:33:12 +0000

    Game can return a new computer player object. Also required the other two classes in the Game class and removed the optional #initialize arguements as they didn't work as a new player object requires a name on initialization

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,19 @@
+require_relative './player'
+require_relative './computer'
+
 class Game
 
-  def initialize(player_one = Player.new)
+  def initialize(player_one, computer_player)
     @player_one = player_one
+    @computer_player = computer_player
   end
 
   def player1
     @player_one
+  end
+
+  def computer
+    @computer_player
   end
 
   def player_choice
--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -2,13 +2,19 @@ require 'game'
 
 describe Game do
 
-  let(:game) { described_class.new(player_one_dbl) }
+  let(:game) { described_class.new(player_one_dbl, computer_player_dbl) }
   let(:player_one_dbl) { double("player_one") }
-  # let(:computer_player) { double("computer_player") }
+  let(:computer_player_dbl) { double("computer_player") }
 
   describe '#player1' do
     it 'should return a player object' do
       expect(game.player1).to eq(player_one_dbl)
+    end
+  end
+
+  describe '#computer' do
+    it 'should return a computer object' do
+      expect(game.computer).to eq(computer_player_dbl)
     end
   end
 
